d35fc8cfa47d12cfbe5942a301353fe72ad313a7
Cheryl Moser <63269123+moserc@users.noreply.github.com>
2024-02-08 15:50:38 -0800

    Update DynamicTest.java

--- test/Knapsack01/DynamicTest.java
+++ test/Knapsack01/DynamicTest.java
@@ -1,25 +1,16 @@
 package Knapsack01;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-
-/**
- * @author Cheryl Moser
- */
 public class DynamicTest {
 
-    /**
-     * Unit test for Knapsack 0-1, dynamic programming method. Test input
-     * includes a maximum knapsack 'weight', and a 2D array of four items
-     * input[0] = item values; input[1] = item weights.
-     * @author Cheryl Moser
-     */
+       //TEMPLATE FILE
+
+    /*
     @Test
-    public void dynamic() {
-        int maxWeight = 50;
-        int[][] input = {{12, 5, 13, 4}, {20, 40, 16, 9}};
-        int expected = 29;
-        int result = Dynamic.dynamic(input, maxWeight);
-        assertEquals(expected, result);
+    void methodNameHere() {
+	//test input
+	//expected output
+	//method call
+	//assertion
     }
+    */
 }
