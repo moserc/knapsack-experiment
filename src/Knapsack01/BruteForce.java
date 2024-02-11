0026aec264d5df1d83299f77b4216cc8b54afb38
Ashtin <ashtinrivada@yahoo.com>
2024-02-10 19:05:37 -0800

    added base of method for greedy and brute force of 01knapsack

--- src/Knapsack01/BruteForce.java
+++ src/Knapsack01/BruteForce.java
@@ -2,4 +2,16 @@ package Knapsack01;
 
 public class BruteForce {
 
+    public static double bruteForceKnapsack01(int[][] data, int capacity, int items){
+        double startTime = System.nanoTime();
+
+
+
+
+        double endTime = System.nanoTime();
+        double timeTaken = (endTime-startTime) / 1000;
+
+        return timeTaken;
+    }
+
 }
