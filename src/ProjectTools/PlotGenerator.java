4dd3e2d54f13dddbf7d8d49673947e4da736fd62
Cheryl Moser <cherylmomo@gmail.com>
2024-02-12 23:10:00 -0800

    plot class, instantiation in main, image folder w/ test images

---
+++ src/ProjectTools/PlotGenerator.java
@@ -0,0 +1,71 @@
+package ProjectTools;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Class for generating plot charts.
+ * Uses JFreeChart library, included in root folder.
+ * @author Cheryl Moser
+ */
+public class PlotGenerator {
+
+    private final String title;
+    private final String[] arraySize = {"5", "10", "15", "20", "25", "30"};
+    private final String xLabel = "Input Array Size";
+    private final String yLabel = "Time (in microseconds)";
+    private final int width = 800;
+    private final int height = 600;
+
+    //default constructor
+    public PlotGenerator(String title){
+        this.title = title;
+    }
+
+    /**
+     * Creates a line chart using values from a list of recorded execution times
+     * (y-axis), and input size (x-axis).
+     * @param times List of 6 execution times correlating to 6 array sizes.
+     * @param fileName User-specified file path for jpg output.
+     * @author Cheryl Moser
+     */
+    public void lineChart(double[] times, String fileName){
+
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        //iterate through the list of time(ms) values
+        for (int i = 0; i < times.length; i++) {
+            //add data point to chart
+            dataset.addValue(times[i], "Data", arraySize[i]);
+        }
+
+        //create line chart using data points
+        JFreeChart chart = ChartFactory.createLineChart(title, xLabel, yLabel, dataset, PlotOrientation.VERTICAL, false, true, true);
+        //set auto range to include a range containing x-values (may not work in IDE GUI)
+        chart.getCategoryPlot().getRangeAxis().setAutoRange(true);
+        //call to method for generating image files
+        toImage(chart, fileName);
+    }
+
+    /**
+     * Generates a plot image file (jpg).
+     * @param chart The chart generated from xy data points.
+     * @param fileName User-specified file path for jpg output.
+     * @author Cheryl Moser
+     */
+    private void toImage(JFreeChart chart, String fileName){
+        try {
+            //create file & save as jpg
+            File plot = new File(fileName);
+            ChartUtilities.saveChartAsJPEG(plot, chart, width, height);
+        } catch (IOException e){
+            e.getMessage();
+        }
+    }
+}
