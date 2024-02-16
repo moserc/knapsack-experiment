6af4e646f2f899a811cfd92535bd235c929e785d
Cheryl Moser <cherylmomo@gmail.com>
2024-02-15 16:16:27 -0800

    k01 greedy alg + associated unit test

--- src/Knapsack01/Greedy.java
+++ src/Knapsack01/Greedy.java
@@ -1,5 +1,85 @@
 package Knapsack01;
 
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * Class containing the method for performing a greedy solution to the
+ * Knapsack 0-1 problem.
+ * @author Cheryl Moser
+ */
 public class Greedy {
 
-}
+    /**
+     * The greedy method for solving the Knapsack 0-1 problem. This method is
+     * not guaranteed to return optimal results, especially with larger input
+     * arrays. The associated JUnit tests confirm that with an input array of
+     * size 30, it does not report the optimal solution; however, with an array
+     * of size 5, it does. I used an <a href="https://augustineaykara.github.io/Knapsack-Calculator/">
+     * online K01 calculator</a> to retrieve the optimal solution to the input
+     * array proposed in testGreedyLarge. The reason for this is because the
+     * greedy method does not consider combinations of items; instead, it puts
+     * items into the knapsack sequentially by highest value until the weight
+     * limit is met.
+     *
+     * @param weights A list of weights for each item in the knapsack.
+     * @param values A list of values for each item in the knapsack.
+     * @param limit The capacity of the knapsack.
+     * @return runtime in microseconds.
+     * @author Cheryl Moser
+     */
+    public static double greedy(int[] weights, int[] values, int limit) {
+
+        //capture start time
+        long t0 = System.nanoTime();
+
+        //create a priority queue to hold items, sort in decreasing order by
+        //item benefit (key)
+        PriorityQueue<Map.Entry<Integer,Integer>> items = new PriorityQueue<>(
+                //custom comparator via lambda expression
+                (a,b) -> b.getKey() - a.getKey()
+        );
+
+        //add values and weights as key:value pair to priority queue
+        for (int i = 0; i < weights.length; i++){//O(n)
+            items.offer(Map.entry(values[i], weights[i]));
+        }
+
+        //initialize the total weight and value counts
+        int totWeight = 0;
+        int totValue = 0;
+
+        //iterate through the queue until it is empty, or until the weight
+        //limit is met
+        while (!items.isEmpty() && totWeight < limit){
+
+            //dequeue in sorted order (use poll, not remove)
+            Map.Entry<Integer, Integer> item = items.poll();
+
+            int itemWeight = item.getValue();
+
+            //if the added item weight is less than or equal to the limit, add it
+            //otherwise, move on to the next item
+            if (totWeight+itemWeight <= limit){
+                //update total value
+                totValue += item.getKey();
+                //update total weight
+                totWeight += itemWeight;
+            }
+        }
+
+        //capture end time
+        long t1 = System.nanoTime();
+        //convert to microseconds
+        double runtime = (t1 - t0)/1000.0;
+
+        //report total weight and total value of the knapsack
+        System.out.println("\nK01-greedy:\nTotal Weight = "+totWeight+
+                "\nTotal Value = "+totValue+"\n");
+        System.out.println("Runtime: "+runtime+"ms");
+
+        //return runtime
+        return runtime;
+
+    }//end greedy
+}//end class
--- test/Knapsack01/GreedyTest.java
+++ test/Knapsack01/GreedyTest.java
@@ -1,16 +1,97 @@
 package Knapsack01;
 
-class GreedyTest {
+import org.junit.Test;
 
-    //TEMPLATE FILE
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
-    /*
+import static Knapsack01.Greedy.greedy;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit tests for the K01 greedy method.
+ * I used an <a href="https://augustineaykara.github.io/Knapsack-Calculator/">
+ * online K01 calculator</a> to affirm the expected optimal solution values.
+ * @author Cheryl Moser
+ */
+public class GreedyTest {
+
+    /**
+     * Tests the K01 greedy method with a small input array.
+     * Asserts a reasonable runtime value, and asserts correct optimal
+     * weight and value results of the knapsack (values captured from the print
+     * stream of System.out).
+     * @author Cheryl Moser
+     */
     @Test
-    void methodNameHere() {
-	//test input
-	//expected output
-	//method call
-	//assertion
-    }
-    */
-}
+    public void testGreedySmall(){
+
+        //define input array of size 5
+        int[] values = {10,16,50,8,32};
+        int[] weights = {14,4,9,20,2};
+        int limit = 30;
+
+        //set up to capture the printed output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        PrintStream originalOut = System.out;
+        System.setOut(printStream);
+
+        //call greedy
+        double runtime = greedy(weights, values, limit);
+
+        //reset System.out
+        System.out.flush();
+        System.setOut(originalOut);
+
+        //grab weight and benefit values from captured output
+        String printedOutput = outputStream.toString();
+
+        //assertions
+        assertTrue(runtime > 0);
+        assertTrue(printedOutput.contains("Total Weight = 29")); //expected --> 29
+        assertTrue(printedOutput.contains("Total Value = 108")); //expected --> 108
+
+    }//end small greedy test
+
+    /**
+     * Tests the K01 greedy method with a large input array.
+     * Asserts a reasonable runtime value, and asserts that the
+     * weight and value results of the knapsack (values captured from the print
+     * stream of System.out) will not be the optimal solution.
+     * @author Cheryl Moser
+     */
+    @Test
+    public void testGreedyLarge(){
+
+        //define input array of size 30
+        int[] values = {10,16,50,8,32,40,26,2,102,55,48,37,23,1,67,12,5,6,21,20,33,41,49,9,7,3,11,13,18,4};
+        int[] weights = {14,4,9,20,2,3,5,12,10,13,30,29,28,17,18,6,27,15,11,19,1,7,8,16,21,23,4,22,3,2};
+        int limit = 200;
+
+        //set up to capture the printed output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        PrintStream originalOut = System.out;
+        System.setOut(printStream);
+
+        //call greedy
+        double runtime = greedy(weights, values, limit);
+
+        //reset System.out
+        System.out.flush();
+        System.setOut(originalOut);
+
+        //grab weight and benefit values from captured output
+        String printedOutput = outputStream.toString();
+
+        //assertions
+        assertTrue(runtime > 0);
+        assertFalse(printedOutput.contains("Total Weight = 198")); //expected optimal--> 198
+        assertFalse(printedOutput.contains("Total Value = 692")); //expected optimal--> 692
+        //optimal values calculated using an online calculator. See source in the
+        //class javadoc above
+
+    }//end large greedy test
+}//end class
