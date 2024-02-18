5279ff1bdf84e0927cf636520c7c971deaf50c3f
Ashtin <ashtinrivada@yahoo.com>
2024-02-18 15:45:50 -0800

    cleaned up a few inconsistencies in my brute force method, and added a test for my brute force method

--- test/Knapsack01/BruteForceTest.java
+++ test/Knapsack01/BruteForceTest.java
@@ -3,26 +3,34 @@ package Knapsack01;
 import ProjectTools.Knapsack;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BruteForceTest {
 
 
-//    @Test
-//    public void bruteforce01Test(){
-//        int[] testVals = {1,2,3};
-//        int[] testWeights = {3,2,1};
-//        assertEquals(Knapsack01.BruteForce.bruteForceKnapsack01(testWeights, testVals, 3, 0), );
-//    }
-    //TEMPLATE FILE
-
-    /*
+    /**
+     * Test that will check if output is correct
+     */
     @Test
-    void methodNameHere() {
-	//test input
-	//expected output
-	//method call
-	//assertion
+    public void bruteforce01Test(){
+        int[] testVals = {1,2,3};
+        int[] testWeights = {3,2,1};
+        Knapsack testSack = new Knapsack(0, 3, 3, testVals, testWeights);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+
+        Knapsack01.BruteForce.bruteForceKnapsack01(testSack.getWeights(), testSack.getValues(), 3, 0);
+
+
+        String output = outputStream.toString();
+        assertTrue(output.contains("Values in this knapsack are: [1, 2, 3]"));
+        assertTrue(output.contains("Weights in this knapsack are: [3, 2, 1]"));
+        assertTrue(output.contains("Total Weight:  3"));
+        assertTrue(output.contains("Total Profit:  5"));
+
     }
-    */
 }
