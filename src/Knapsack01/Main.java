13f536b93fdfbadcaa68eaa6747b758fde0cbd86
Ashtin <ashtinrivada@yahoo.com>
2024-02-18 10:24:07 -0800

    Deleted import Drawable

--- src/Knapsack01/Main.java
+++ src/Knapsack01/Main.java
@@ -2,6 +2,8 @@ package Knapsack01;
 
 import ProjectTools.PlotGenerator;
 
+import java.util.Arrays;
+
 public class Main {
 	public static void main(String[] args) {
 
@@ -11,21 +13,22 @@ public class Main {
 		--------------------PLOT GENERATION:--------------------
 		*/
 		//chart instantiation
-		PlotGenerator k01BruteForceChart = new PlotGenerator("Knapsack 0/1 - Brute Force");
-		PlotGenerator k01GreedyChart = new PlotGenerator("Knapsack 0/1 - Greedy");
-		PlotGenerator k01DPChart = new PlotGenerator("Knapsack 0/1 - Dynamic Programming");
-
-		//the line below is arbitrary and for testing-only, replace with a
-		//double[] holding actual times for each algorithm
-		double[] times = {10.2, 15.5, 20.1, 25.3, 30.2, 40.8};
-
-		//chart construction and jpg output
-		k01BruteForceChart.lineChart(times, "./images/k01_bruteForce.jpg");
-		k01GreedyChart.lineChart(times, "./images/k01_greedy.jpg");
-		k01DPChart.lineChart(times, "./images/k01_dp.jpg");
+//		PlotGenerator k01BruteForceChart = new PlotGenerator("Knapsack 0/1 - Brute Force");
+//		PlotGenerator k01GreedyChart = new PlotGenerator("Knapsack 0/1 - Greedy");
+//		PlotGenerator k01DPChart = new PlotGenerator("Knapsack 0/1 - Dynamic Programming");
+//
+//		//the line below is arbitrary and for testing-only, replace with a
+//		//double[] holding actual times for each algorithm
+//		double[] times = {10.2, 15.5, 20.1, 25.3, 30.2, 40.8};
+//
+//		//chart construction and jpg output
+//		k01BruteForceChart.lineChart(times, "./images/k01_bruteForce.jpg");
+//		k01GreedyChart.lineChart(times, "./images/k01_greedy.jpg");
+//		k01DPChart.lineChart(times, "./images/k01_dp.jpg");
 		/*
 		--------------------END PLOT GENERATION:--------------------
 		*/
+
 
 		//for all below, I'm just making a wild guess as to what data structures we'll be using
 		/*
