fa05ff4d2ce685eca1d141325c6e21385e963140
Ashtin <ashtinrivada@yahoo.com>
2024-02-18 11:43:18 -0800

    Fixed small bugs in csvReader, also fixed up the paths for the files, and have an example for output using bruteforce for knapsack01.

--- test/Knapsack01/BruteForceTest.java
+++ test/Knapsack01/BruteForceTest.java
@@ -1,7 +1,19 @@
 package Knapsack01;
 
+import ProjectTools.Knapsack;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class BruteForceTest {
 
+
+//    @Test
+//    public void bruteforce01Test(){
+//        int[] testVals = {1,2,3};
+//        int[] testWeights = {3,2,1};
+//        assertEquals(Knapsack01.BruteForce.bruteForceKnapsack01(testWeights, testVals, 3, 0), );
+//    }
     //TEMPLATE FILE
 
     /*
